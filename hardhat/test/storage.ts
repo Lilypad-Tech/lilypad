1a7f678cc6c7feea02bf425490f657052c1562e8
Kai Davenport <kaiyadavenport@gmail.com>
2023-08-28 08:50:59 +0100

    use the fixture loading function and now the tests are much faster

--- hardhat/test/storage.ts
+++ hardhat/test/storage.ts
@@ -22,9 +22,52 @@ const { expect } = chai
 
 describe("Storage", () => {
 
+  const rpCID = ethers.getBigInt(123)
+  const rpURL = "abc"
+
+  const jcCID = ethers.getBigInt(456)
+  const jcURL = "def"
+
   async function setupStorage() {
     const admin = getWallet('admin')
     const storage = await deployStorage(admin)
+    return storage
+  }
+
+  async function setupStorageWithUsers() {
+    const storage = await setupStorage()
+    expect(await storage
+      .connect(getWallet('resource_provider'))
+      .updateUser(
+        rpCID,
+        rpURL,
+        [
+          getServiceType('ResourceProvider'),
+        ],
+        [
+          getAddress('mediator')
+        ],
+        [
+          getAddress('directory')
+        ]
+      )
+    ).to.not.be.reverted
+    expect(await storage
+      .connect(getWallet('job_creator'))
+      .updateUser(
+        jcCID,
+        jcURL,
+        [
+          getServiceType('JobCreator'),
+        ],
+        [
+          getAddress('mediator')
+        ],
+        [
+          getAddress('directory')
+        ]
+      )
+    ).to.not.be.reverted
     return storage
   }
 
@@ -39,52 +82,8 @@ describe("Storage", () => {
 
   describe("Users", () => {
 
-    const rpCID = ethers.getBigInt(123)
-    const rpURL = "abc"
-
-    const jcCID = ethers.getBigInt(456)
-    const jcURL = "def"
-
-    const addUsers = async (storage: LilypadStorage) => {
-      expect(await storage
-        .connect(getWallet('resource_provider'))
-        .updateUser(
-          rpCID,
-          rpURL,
-          [
-            getServiceType('ResourceProvider'),
-          ],
-          [
-            getAddress('mediator')
-          ],
-          [
-            getAddress('directory')
-          ]
-        )
-      ).to.not.be.reverted
-
-      expect(await storage
-        .connect(getWallet('job_creator'))
-        .updateUser(
-          jcCID,
-          jcURL,
-          [
-            getServiceType('JobCreator'),
-          ],
-          [
-            getAddress('mediator')
-          ],
-          [
-            getAddress('directory')
-          ]
-        )
-      ).to.not.be.reverted
-    }
-
     it("Should be able to add and get users", async function () {
-      const storage = await loadFixture(setupStorage)
-      
-      await addUsers(storage)
+      const storage = await loadFixture(setupStorageWithUsers)
       
       const rp = await storage.getUser(getAddress('resource_provider'))
       const jc = await storage.getUser(getAddress('job_creator'))
@@ -103,9 +102,8 @@ describe("Storage", () => {
     })
 
     it("Should be able to update an existing user", async function () {
-      const storage = await loadFixture(setupStorage)
-      await addUsers(storage)
-
+      const storage = await loadFixture(setupStorageWithUsers)
+      
       const newCID = ethers.getBigInt(456)
       const newURL = "def"
 
@@ -133,8 +131,7 @@ describe("Storage", () => {
     })
 
     it("Should be able to list users of a certain role", async function () {
-      const storage = await loadFixture(setupStorage)
-      await addUsers(storage)
+      const storage = await loadFixture(setupStorageWithUsers)
 
       expect(await storage.showUsersInList(getServiceType('JobCreator')))
         .to.deep.equal([])
@@ -153,8 +150,7 @@ describe("Storage", () => {
     })
 
     it("Should deny adding a user to a list without the role", async function () {
-      const storage = await loadFixture(setupStorage)
-      await addUsers(storage)
+      const storage = await loadFixture(setupStorageWithUsers)
 
       await expect(storage
         .connect(getWallet('job_creator'))
@@ -165,8 +161,7 @@ describe("Storage", () => {
     })
 
     it("Should deny adding a user to a list twice", async function () {
-      const storage = await loadFixture(setupStorage)
-      await addUsers(storage)
+      const storage = await loadFixture(setupStorageWithUsers)
 
       expect(await storage
         .connect(getWallet('job_creator'))
@@ -184,8 +179,7 @@ describe("Storage", () => {
     })
 
     it("Should allow a user to remove themselves from a list", async function () {
-      const storage = await loadFixture(setupStorage)
-      await addUsers(storage)
+      const storage = await loadFixture(setupStorageWithUsers)
 
       expect(await storage
         .connect(getWallet('job_creator'))
@@ -206,8 +200,7 @@ describe("Storage", () => {
     })
 
     it("Should deny removing a user from a list if they have not added themselves", async function () {
-      const storage = await loadFixture(setupStorage)
-      await addUsers(storage)
+      const storage = await loadFixture(setupStorageWithUsers)
 
       await expect(storage
         .connect(getWallet('job_creator'))
@@ -215,6 +208,15 @@ describe("Storage", () => {
           getServiceType('JobCreator')
         )
       ).to.be.revertedWith('User is not part of that list')
+    })
+
+  })
+
+  describe("Deals", () => {
+
+    it("Should be able to add a deal and then read it", async function () {
+
+
     })
 
   })
