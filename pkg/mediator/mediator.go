f0f0d10f4873bb1e7046135e4b6fdfcd8636e1c1
Kai Davenport <kaiyadavenport@gmail.com>
2023-09-08 18:58:23 +0100

    add mediator service based on resource provider

---
+++ pkg/mediator/mediator.go
@@ -0,0 +1,36 @@
+package mediator
+
+import (
+	"context"
+
+	"github.com/bacalhau-project/lilypad/pkg/system"
+	"github.com/bacalhau-project/lilypad/pkg/web3"
+)
+
+type MediatorOptions struct {
+	Web3 web3.Web3Options
+}
+
+type Mediator struct {
+	web3SDK    *web3.ContractSDK
+	controller *MediatorController
+}
+
+func NewResourceProvider(
+	options MediatorOptions,
+	web3SDK *web3.ContractSDK,
+) (*Mediator, error) {
+	controller, err := NewMediatorController(web3SDK)
+	if err != nil {
+		return nil, err
+	}
+	solver := &Mediator{
+		controller: controller,
+		web3SDK:    web3SDK,
+	}
+	return solver, nil
+}
+
+func (mediator *Mediator) Start(ctx context.Context, cm *system.CleanupManager) error {
+	return mediator.controller.Start(ctx, cm)
+}
