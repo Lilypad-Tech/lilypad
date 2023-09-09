f8ae040ead485eb23d970d6453959ac491f4d0f5
Kai Davenport <kaiyadavenport@gmail.com>
2023-09-09 17:25:48 +0100

    load the solver private key in the intergration test

--- test/integration_test.go
+++ test/integration_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"os"
 	"testing"
 
 	optionsfactory "github.com/bacalhau-project/lilypad/pkg/options"
@@ -9,6 +10,12 @@ import (
 
 func TestStack(t *testing.T) {
 	solverOptions := optionsfactory.NewSolverOptions()
+	solverOptions.Web3.PrivateKey = os.Getenv("SOLVER_PRIVATE_KEY")
+
+	// test that the solver private key is defined
+	if solverOptions.Web3.PrivateKey == "" {
+		t.Error("SOLVER_PRIVATE_KEY is not defined")
+	}
 
 	spew.Dump(solverOptions)
 }
