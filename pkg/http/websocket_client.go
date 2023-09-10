cdd89946d77a143bb7b7970d88de1041be98f6c1
Kai Davenport <kaiyadavenport@gmail.com>
2023-09-10 17:19:27 +0100

    enable ctrl+c to quit a looping reconnecting websocket

--- pkg/http/websocket_client.go
+++ pkg/http/websocket_client.go
@@ -15,13 +15,32 @@ func ConnectWebSocket(
 	ctx context.Context,
 ) *websocket.Conn {
 	closed := false
+
 	var conn *websocket.Conn
+
+	go func() {
+		for {
+			select {
+			case <-ctx.Done():
+				closed = true
+				if conn != nil {
+					conn.Close()
+				}
+
+				return
+			}
+		}
+	}()
+
 	for {
 		var err error
 		log.Info().Msgf("WebSocket connection connecting: %s", url)
 		conn, _, err = websocket.DefaultDialer.Dial(url, nil)
 		if err != nil {
 			log.Error().Msgf("WebSocket connection failed: %s\nReconnecting in 2 seconds...", err)
+			if closed {
+				break
+			}
 			time.Sleep(2 * time.Second)
 			continue
 		}
@@ -47,17 +66,6 @@ func ConnectWebSocket(
 					Str("payload", string(p)).
 					Msgf("")
 				messageChan <- p
-			}
-		}
-	}()
-
-	go func() {
-		for {
-			select {
-			case <-ctx.Done():
-				closed = true
-				conn.Close()
-				return
 			}
 		}
 	}()
