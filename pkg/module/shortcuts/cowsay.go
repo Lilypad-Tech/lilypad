6acc61ffc83e24b24594ea7ba91073d408d58818
Kai Davenport <kaiyadavenport@gmail.com>
2023-09-11 19:25:21 +0100

    point at the new shortcut for cowsay v0.0.1

--- pkg/module/shortcuts/cowsay.go
+++ pkg/module/shortcuts/cowsay.go
@@ -9,8 +9,8 @@ func getCowsayVersions() (string, map[string]data.ModuleConfig, error) {
 	versions := map[string]data.ModuleConfig{
 		"v0.0.1": {
 			Repo: "https://github.com/bacalhau-project/lilypad.git",
-			Hash: "abe5eb1033e2c3cadf723960e501a0cbe29d3116",
-			Path: "modules/cowsay.json",
+			Hash: "2e7aae5a5ef4956fe0ee2fde7633e7966f977afc",
+			Path: "module_templates/cowsay.json",
 		},
 	}
 	return latestVersion, versions, nil
