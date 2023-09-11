daba778170544984ed30cde55183074d0dd86fcf
Kai Davenport <kaiyadavenport@gmail.com>
2023-09-11 15:49:26 +0100

    parse LOG_LEVEL as pass it to zerolog

--- pkg/system/log.go
+++ pkg/system/log.go
@@ -10,5 +10,10 @@ import (
 
 func SetupLogging() {
 	output := zerolog.ConsoleWriter{Out: os.Stdout, TimeFormat: time.RFC3339}
-	log.Logger = log.Output(output).With().Caller().Logger()
+	logLevel := zerolog.InfoLevel
+	parsedLogLevel, err := zerolog.ParseLevel(os.Getenv("LOG_LEVEL"))
+	if err == nil {
+		logLevel = parsedLogLevel
+	}
+	log.Logger = log.Output(output).With().Caller().Logger().Level(logLevel)
 }
