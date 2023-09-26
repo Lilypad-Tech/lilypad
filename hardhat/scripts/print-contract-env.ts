d7faf417f392175996734e4032e5c25b2738096f
Kai Davenport <kaiyadavenport@gmail.com>
2023-09-26 00:08:02 +0000

    fun faucet script

--- hardhat/scripts/print-contract-env.ts
+++ hardhat/scripts/print-contract-env.ts
@@ -1,10 +1,13 @@
 import {
   getControllerAddress,
+  getTokenAddress,
 } from '../utils/web3'
 
 async function main() {
   const controllerAddress = await getControllerAddress()
+  const tokenAddress = await getTokenAddress()
   console.log(`export WEB3_CONTROLLER_ADDRESS=${controllerAddress}`)
+  console.log(`export WEB3_TOKEN_ADDRESS=${tokenAddress}`)
 }
 
 main().catch((error) => {
