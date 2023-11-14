e7a2f8fe33d314b43ab102ce1a62b165ad0de9d7
Holly Grimm <holly.grimm@gmail.com>
2023-11-14 08:09:06 -0700

    mumbai deployments

--- hardhat/hardhat.config.ts
+++ hardhat/hardhat.config.ts
@@ -33,6 +33,10 @@ const config: HardhatUserConfig = {
       url: `https://sepolia.infura.io/v3/${INFURA_KEY}`,
       accounts: PRIVATE_KEYS,
     },
+    mumbai: {
+      url: `https://polygon-mumbai.infura.io/v3/${INFURA_KEY}`,
+      accounts: PRIVATE_KEYS,
+    },
   },
   etherscan: {
     apiKey: process.env.ETHERSCAN_API_KEY,
