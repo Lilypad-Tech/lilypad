2f37e9d1c7c3d9ce253dbcbf53911064acbd66a3
Nasfame <laciferin@gmail.com>
2023-11-29 03:40:01 +0530

    go : generate
    Signed-off-by: Nasfame <laciferin@gmail.com>
    
    Took 21 minutes

---
+++ generate.go
@@ -0,0 +1,4 @@
+package main
+
+// //go:generate echo \"go build -ldflags="-X lilypad.VERSION=$(git describe --tags --abbrev=0) -X lilypad.COMMIT_SHA=$(git rev-parse HEAD)"\"
+//go:generate echo \"go build -ldflags="-X lilypad.VERSION=$(git describe --tags --abbrev=0) -X lilypad.COMMIT_SHA=$(git rev-parse HEAD)"\"
