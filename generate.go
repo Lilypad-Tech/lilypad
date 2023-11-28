dd247179177c07155978d09877608aa17bcb0e88
Nasfame <laciferin@gmail.com>
2023-11-29 03:43:56 +0530

    build: ignore
    Signed-off-by: Nasfame <laciferin@gmail.com>
    
    Took 4 minutes

--- generate.go
+++ generate.go
@@ -1,4 +1,6 @@
+//go:build ignore
 package main
 
-// //go:generate echo \"go build -ldflags="-X lilypad.VERSION=$(git describe --tags --abbrev=0) -X lilypad.COMMIT_SHA=$(git rev-parse HEAD)"\"
-//go:generate echo \"go build -ldflags="-X lilypad.VERSION=$(git describe --tags --abbrev=0) -X lilypad.COMMIT_SHA=$(git rev-parse HEAD)"\"
+go:generate echo \"go build -ldflags="-X lilypad.VERSION=$(git describe --tags --abbrev=0) -X lilypad.COMMIT_SHA=$(git rev-parse HEAD)"\" | exec $0
+
+go:generate go build -ldflags="-X lilypad.VERSION=$(git describe --tags --abbrev=0) -X lilypad.COMMIT_SHA=$(git rev-parse HEAD)"
