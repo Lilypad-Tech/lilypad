16b78c726729ae4e7389551a4c13609f236aba25
Nasfame <laciferin@gmail.com>
2023-11-29 01:04:01 +0530

    fixes: boiler plate
    Signed-off-by: Nasfame <laciferin@gmail.com>
    
    Took 7 minutes

--- cmd/lilypad/version.go
+++ cmd/lilypad/version.go
@@ -6,26 +6,23 @@ import (
 	"github.com/spf13/cobra"
 
 	optionsfactory "github.com/bacalhau-project/lilypad/pkg/options"
-	"github.com/bacalhau-project/lilypad/pkg/solver"
 	"github.com/bacalhau-project/lilypad/pkg/system"
 )
 
+// VERSION: use `go build -ldflags="-X lilypad.VERSION=x.y.z" `
 const VERSION = ""
 
 func newVersionCmd() *cobra.Command {
 	options := optionsfactory.NewSolverOptions()
 
 	versionCmd := &cobra.Command{
-		Use:     "solver",
-		Short:   "Start the lilypad solver service.",
-		Long:    "Start the lilypad solver service.",
-		Example: "",
+		Use:     "version",
+		Aliases: []string{"-v"},
+		Short:   "Get the lilypad version",
+		Long:    "Get the lilypad version",
+		Example: "lilypad version",
 		RunE: func(cmd *cobra.Command, _ []string) error {
-			options, err := optionsfactory.ProcessSolverOptions(options)
-			if err != nil {
-				return err
-			}
-			return runVersion(cmd, options)
+			return runVersion(cmd)
 		},
 	}
 
@@ -34,7 +31,7 @@ func newVersionCmd() *cobra.Command {
 	return versionCmd
 }
 
-func runVersion(cmd *cobra.Command, options solver.SolverOptions) error {
+func runVersion(cmd *cobra.Command) error {
 	commandCtx := system.NewCommandContext(cmd)
 	defer commandCtx.Cleanup()
 
