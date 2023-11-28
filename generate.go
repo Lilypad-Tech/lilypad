ec3fc8809537c1d3bb68cc6982bc07de489ca89e
Nasfame <laciferin@gmail.com>
2023-11-29 03:49:25 +0530

    generate:
    Signed-off-by: Nasfame <laciferin@gmail.com>
    
    Took 5 minutes

--- generate.go
+++ generate.go
@@ -1,6 +1,11 @@
-//go:build ignore
+//go:build generate
+
 package main
 
-go:generate echo \"go build -ldflags="-X lilypad.VERSION=$(git describe --tags --abbrev=0) -X lilypad.COMMIT_SHA=$(git rev-parse HEAD)"\" | exec $0
+//go:generate echo "go build -ldflags=\"-X lilypad.VERSION=$(git describe --tags --abbrev=0) -X lilypad.COMMIT_SHA=$(git rev-parse HEAD)\" ." | tee /dev/tty | sh
+
+
+//go:generate ./lilypad -v
+
 
 go:generate go build -ldflags="-X lilypad.VERSION=$(git describe --tags --abbrev=0) -X lilypad.COMMIT_SHA=$(git rev-parse HEAD)"
