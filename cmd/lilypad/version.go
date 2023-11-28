58b9842880f55ee6262828879c5bd9304d5e87be
Nasfame <laciferin@gmail.com>
2023-11-29 01:37:37 +0530

    lilypad: TODO:
    Signed-off-by: Nasfame <laciferin@gmail.com>
    
    Took 2 minutes

--- cmd/lilypad/version.go
+++ cmd/lilypad/version.go
@@ -12,6 +12,9 @@ import (
 // VERSION use `go build -ldflags="-X lilypad.VERSION=vX.Y.Z" `
 var VERSION string
 
+// COMMIT_SHA use `go build -ldflags="-X lilypad.COMMIT_SHA=$(git rev-parse HEAD)" `
+var COMMIT_SHA string
+
 const GO_BINARY_URL = "https://github.com/bacalhau-project/lilypad/releases/"
 
 func newVersionCmd() *cobra.Command {
@@ -44,6 +47,7 @@ func runVersion(cmd *cobra.Command) error {
 	}
 
 	fmt.Printf("Lilypad: %s\n", VERSION)
+	fmt.Printf("Commit: %s\n", COMMIT_SHA)
 
 	// TODO: suggest updating to the latest version if the current version is not the latest version
 
