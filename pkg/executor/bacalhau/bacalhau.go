8e8292322c4c01cccafd4b2c1f1101205986265d
James Walker <walkah@walkah.net>
2024-06-27 16:13:09 -0400

    fix: log output errors from bacalhau id

--- pkg/executor/bacalhau/bacalhau.go
+++ pkg/executor/bacalhau/bacalhau.go
@@ -51,11 +51,17 @@ func (executor *BacalhauExecutor) Id() (string, error) {
 
 	runOutputRaw, err := nodeIdCmd.CombinedOutput()
 	if err != nil {
-		return "", fmt.Errorf("error calling get id results %s", err.Error())
+		return "", fmt.Errorf("error calling get id results %s, %s", err.Error(), runOutputRaw)
 	}
 
-	splitOutputs := strings.Split(strings.Trim(string(runOutputRaw), " \t\n"), "\n")
+	splitOutputs := strings.Split(string(runOutputRaw), "\n")
 	runOutput := splitOutputs[len(splitOutputs)-1]
+	outputError := strings.Join(strings.Fields(strings.Join(splitOutputs[:len(splitOutputs)-1], " ")), " ")
+	if outputError != "" {
+		// TODO: we need to figure out if errors here are fatal or not
+		log.Warn().Msgf("error calling bacalhau id: %s", outputError)
+	}
+
 	var idResult struct {
 		ID       string
 		ClientID string
