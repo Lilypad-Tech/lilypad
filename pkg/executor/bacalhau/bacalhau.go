29e12fdf1701c81821b2ee982014e5e2be9790fe
江子牙 <jw19961019@icloud.com>
2024-06-29 09:56:50 +0800

    * fix: json unmarshalling error bacalhau id
    json content is empty, so error

--- pkg/executor/bacalhau/bacalhau.go
+++ pkg/executor/bacalhau/bacalhau.go
@@ -55,12 +55,10 @@ func (executor *BacalhauExecutor) Id() (string, error) {
 	}
 
 	splitOutputs := strings.Split(string(runOutputRaw), "\n")
-	runOutput := splitOutputs[len(splitOutputs)-1]
-	outputError := strings.Join(strings.Fields(strings.Join(splitOutputs[:len(splitOutputs)-1], " ")), " ")
-	if outputError != "" {
-		// TODO: we need to figure out if errors here are fatal or not
-		log.Warn().Msgf("error calling bacalhau id: %s", outputError)
+	if len(splitOutputs) == 0 {
+		log.Fatalf("error calling get id results %s, %s", err.Error(), runOutputRaw)
 	}
+	runOutput := splitOutputs[0]
 
 	var idResult struct {
 		ID       string
