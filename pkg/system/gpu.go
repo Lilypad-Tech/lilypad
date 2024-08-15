5294d4741b43b580bfdbe620cb9c830a223382f6
Brian Ginsburg <gins@brianginsburg.com>
2024-08-15 10:15:50 -0700

    chore: Fix GPU info nil pointer exceptions

--- pkg/system/gpu.go
+++ pkg/system/gpu.go
@@ -1,8 +1,6 @@
 package system
 
 import (
-	"fmt"
-
 	"github.com/jaypipes/ghw"
 	"github.com/rs/zerolog/log"
 )
@@ -16,10 +14,7 @@ func GetGPUInfo() []string {
 	var gpu_info []string
 	if gpu != nil {
 		for _, card := range gpu.GraphicsCards {
-			gpu_info = append(
-				gpu_info,
-				fmt.Sprintf("%s %s (%s)", card.DeviceInfo.Vendor.Name, card.DeviceInfo.Product.Name, card.DeviceInfo.Driver),
-			)
+			gpu_info = append(gpu_info, card.String())
 		}
 	}
 
