f71b16fbb9852729d230550a58f713d673ff8370
mcryptographer <mcryptographer@gmail.com>
2024-10-18 04:12:34 +0800

    feat: DB models

--- pkg/solver/store/sqlite/models.go
+++ pkg/solver/store/sqlite/models.go
@@ -7,9 +7,9 @@ import (
 
 // this is what the solver keeps track of so we can know
 // what the current state of the deal is
-type JobOfferContainer struct {
+type JobOfferData struct {
 	gorm.Model
-	ID         string   `json:"id"`
+	CID         string   `json:"cid"`
 	DealID     string   `json:"deal_id"`
 	JobCreator string   `json:"job_creator"`
 	State      uint8    `json:"state"`
@@ -18,33 +18,33 @@ type JobOfferContainer struct {
 
 // this is what the solver keeps track of so we can know
 // what the current state of the deal is
-type ResourceOfferContainer struct {
+type ResourceOfferData struct {
 	gorm.Model
-	ID               string        `json:"id"`
-	DealID           string        `json:"deal_id"`
-	ResourceProvider string        `json:"resource_provider"`
-	State            uint8         `json:"state"`
-	ResourceOffer    data.ResourceOffer `json:"job_offer"`
+	CID               string				`json:"cid"`
+	DealID           string        			`json:"deal_id"`
+	ResourceProvider string        			`json:"resource_provider"`
+	State            uint8         			`json:"state"`
+	ResourceOffer    data.ResourceOffer		`json:"job_offer"`
 }
 
-type DealContainer struct {
+type DealData struct {
 	gorm.Model
-	ID               string           `json:"id"`
-	JobCreator       string           `json:"job_creator"`
-	ResourceProvider string           `json:"resource_provider"`
-	JobOffer         string           `json:"job_offer"`
-	ResourceOffer    string           `json:"resource_offer"`
-	State            uint8            `json:"state"`
-	Deal             data.Deal             `json:"deal"`
-	Transactions     data.DealTransactions `json:"transactions"`
-	Mediator         string           `json:"mediator"`
+	CID               string				`json:"cid"`
+	JobCreator       string           		`json:"job_creator"`
+	ResourceProvider string           		`json:"resource_provider"`
+	JobOffer         string           		`json:"job_offer"`
+	ResourceOffer    string           		`json:"resource_offer"`
+	State            uint8            		`json:"state"`
+	Deal             data.Deal				`json:"deal"`
+	Transactions     data.DealTransactions	`json:"transactions"`
+	Mediator         string					`json:"mediator"`
 }
 
-type Result struct {
+type ResultData struct {
 	gorm.Model
 	// this is the cid of the result where ID is set to empty string
-	ID     string `json:"id"`
-	DealID string `json:"deal_id"`
+	CID     string			`json:"cid"`
+	DealID string			`json:"deal_id"`
 	// the CID of the actual results
 	DataID           string `json:"results_id"`
 	Error            string `json:"error"`
@@ -54,7 +54,7 @@ type Result struct {
 // represents a solver decision
 // the solver keeps track of "no" decisions to avoid trying to repeatedly match
 // things it's already decided it can't match
-type MatchDecision struct {
+type MatchDecisionData struct {
 	gorm.Model
 	JobOffer      string `json:"job_offer"`
 	ResourceOffer string `json:"resource_offer"`
