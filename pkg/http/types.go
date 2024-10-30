d65aa0d0b0f3b9cb2e0471c7ecd9315357739fc9
Brian Ginsburg <gins@brianginsburg.com>
2024-10-30 12:00:37 -0700

    chore: Add server rate limiter options

--- pkg/http/types.go
+++ pkg/http/types.go
@@ -1,9 +1,15 @@
 package http
 
 type ServerOptions struct {
-	URL  string
-	Host string
-	Port int
+	URL         string
+	Host        string
+	Port        int
+	RateLimiter RateLimiterOptions
+}
+
+type RateLimiterOptions struct {
+	RequestLimit int
+	WindowLength int
 }
 
 type ClientOptions struct {
