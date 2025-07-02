21e9162a65db60a16b49b27d8b1a65b40b4c2bd1
Narbeh Shahnazarian <me@narb.dev>
2025-07-02 15:58:38 -0700

    adds logging to anura client and fixes bug with unmarshelling

--- pkg/anura/client.go
+++ pkg/anura/client.go
@@ -4,12 +4,19 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
+	"github.com/rs/zerolog/log"
 	"io"
 	"net/http"
 	"time"
 )
 
 type ModuleDiscoveryResponse struct {
+	Data    ResponseData `json:"data"`
+	Message string       `json:"message"`
+	Status  int          `json:"status"`
+}
+
+type ResponseData struct {
 	Modules []Module `json:"modules"`
 }
 
@@ -37,9 +44,18 @@ type anuraClient struct {
 }
 
 func NewAnuraClient(anuraOptions AnuraClientOptions) (AnuraClient, error) {
-	if len(anuraOptions.ApiKey) == 0 || len(anuraOptions.BaseURL) == 0 {
-		return nil, errors.New("anura api key and baseUrl are required")
+	if len(anuraOptions.ApiKey) == 0 {
+		err := errors.New("Anura API key not set")
+		log.Error().Err(err).Msg("Anura API Key is required. You can learn how to obtain one by reading the Anura docs: https://docs.lilypad.tech/lilypad/developer-resources/inference-api")
+		return nil, err
 	}
+
+	if len(anuraOptions.BaseURL) == 0 {
+		err := errors.New("Anura base URL not set")
+		log.Error().Err(err).Msg("Anura Base URL is required. You can learn what the Base URL is by reading the Anura docs: https://docs.lilypad.tech/lilypad/developer-resources/inference-api")
+		return nil, err
+	}
+
 	return &anuraClient{
 		clientOptions: anuraOptions,
 		client:        http.Client{Timeout: time.Duration(30) * time.Second}}, nil
@@ -47,26 +63,31 @@ func NewAnuraClient(anuraOptions AnuraClientOptions) (AnuraClient, error) {
 
 func (a *anuraClient) GetAnuraModules() (ModuleDiscoveryResponse, error) {
 	getModulesUrl := a.clientOptions.BaseURL + "/modules"
+	fmt.Println(getModulesUrl)
 	req, err := http.NewRequest("GET", getModulesUrl, nil)
 	if err != nil {
+		log.Error().Err(err).Msg("Unable to build anura get modules request")
 		return ModuleDiscoveryResponse{}, err
 	}
 
 	req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", a.clientOptions.ApiKey))
 	resp, err := a.client.Do(req)
 	if err != nil {
+		log.Error().Err(err).Msg("Failed anura get modules request")
 		return ModuleDiscoveryResponse{}, err
 	}
 	defer resp.Body.Close()
 
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
+		log.Error().Err(err).Msg("Failed reading get modules response")
 		return ModuleDiscoveryResponse{}, err
 	}
 
 	var response ModuleDiscoveryResponse
 	err = json.Unmarshal(body, &response)
 	if err != nil {
+		log.Error().Err(err).Msg("Failed unmarshelling get modules response")
 		return ModuleDiscoveryResponse{}, err
 	}
 
